fafc609ffee43c01b9efe4ff2aa73a13a600b6b3
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-02 21:06:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from fastapi import FastAPI
 from fastapi_versioning import VersionedFastAPI
+
+app = FastAPI()
+
+
+if __name__ == '__main__':
+    import uvicorn
+    app = VersionedFastAPI(app, version_format="{major}", prefix_format="/api/v{major}")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
