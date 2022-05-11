d9f0bdc5b9c1f21f24c20e557ddcf577a7946177
Super <juwoon7163@gmail.com>
2022-05-11 14:14:44 +0900

    .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from urllib.request import Request
 from fastapi import FastAPI
 from fastapi_versioning import VersionedFastAPI
 import uvicorn
+from loguru import logger
 from routers import main_router, post_router
 
 
@@ -9,15 +11,15 @@ app.include_router(main_router.router)
 app.include_router(post_router.router)
 
 
-@app.get("/masd")
-def main():
-    print("ㅋ")
-    return {"message": "Hello World"}
+@app.middleware("http")
+async def logging_middleware(request: Request, call_next):
+    logger.info(f"{request.method} {request.url}")
+    response = await call_next(request)
+    logger.info(f"{response.status_code} {response.url}")
+    return response
 
 
 if __name__ == "__main__":
-    import os
-
     app = VersionedFastAPI(
         app,
         version_format="{major}",
