0ddd0e6c47be30c2ca4b7261a8d1e1e263dc7ae1
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-26 10:17:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
-from fastapi_versioning import VersionedFastAPI
-import uvicorn
-from routers import main_router, post_router, signup_router
+from fastapi.middleware.cors import CORSMiddleware
+
+from core.config import settings
 
 
-app = FastAPI(title="Veloog", description="Velog clone coding api docs")
-app.include_router(main_router.router)
-app.include_router(post_router.router)
-app.include_router(signup_router.router)
+app = FastAPI(title=settings.PROJECT_NAME)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    app = VersionedFastAPI(
-        app,
-        version_format="{major}",
-        prefix_format="/api/v{major}",
-    )
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    pass
