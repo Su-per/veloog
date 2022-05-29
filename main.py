41602efda926519edfbe22ed52277e9bd4838cf4
Super <juwoon7163@gmail.com>
2022-05-30 08:31:34 +0900

    .

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ from core.config import settings
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 if __name__ == "__main__":
     pass
