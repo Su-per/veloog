880f13f66da9df6fbc56e66db561326fbb45ef64
Super <juwoon7163@gmail.com>
2022-05-30 11:56:06 +0900

    .

--- core/config.py
+++ core/config.py
@@ -1,12 +1,22 @@
 from pydantic import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "VELOOG"
 
-    class Config:
-        case_sensitive = True
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: int = os.getenv("DB_PORT")
+    SQLALCHEMY_DB_URL: str = (
+        f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
 
 
 settings = Settings()
