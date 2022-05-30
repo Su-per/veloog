f6779a9268e7e1690b4afa32e07bde8f0a83f17e
Super <juwoon7163@gmail.com>
2022-05-30 15:48:35 +0900

    .

--- db/session.py
+++ db/session.py
@@ -2,7 +2,6 @@ from core.config import settings
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-
 engine = create_engine(settings.SQLALCHEMY_DB_URL, echo=True)
 Session = sessionmaker()
 Session.configure(bind=engine)
