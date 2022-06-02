07bab51c02e0d6084dfbaa6e8844acae21862feb
이주원 <60950583+Su-per@users.noreply.github.com>
2022-06-02 10:02:24 +0900

    SQLAlchemy -> fastapi-sqlalchemy 로 변경

--- api/main/trending.py
+++ api/main/trending.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter, Depends
-from db.session import get_db
+from fastapi import APIRouter
+from fastapi_sqlalchemy import db
 from models.post import Post
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
 @router.get("/trending")
-async def trending(pages: int = 0, db: Session = Depends(get_db)):
-    print(db)
+async def trending(pages: int = 0):
+    pass
